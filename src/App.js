7c7eadc667c6823228d9c6d2ee3c372d38b6d518
simjinyi <38304422+simjinyi@users.noreply.github.com>
2020-10-03 01:40:42 +0800

    Home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from '@material-ui/core';
-import { red } from '@material-ui/core/colors';
+import { Box, Typography, Button } from "@material-ui/core";
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Box display="flex" flexDirection="column">
+      <Box
+        display="flex"
+        flexGrow={1}
+        p={1}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box display="flex" flexDirection="column">
+          <Typography variant="h1">60 Cents</Typography>
+          <Button variant="contained" color="primary" style={{ margin: 5 }}>
+            Test Button 1
+          </Button>
+          <Button variant="contained" color="primary" style={{ margin: 5 }}>
+            Test Button 2
+          </Button>
+        </Box>
+      </Box>
+      <Box display="flex" p={1}>
+        {"I'm a flexbox container!"}
+      </Box>
+    </Box>
   );
 }
-
-
 
 export default App;

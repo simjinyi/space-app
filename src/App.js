243b87bc4262884a901866f9b1cfe5bd9f17956b
wytan1300 <72270643+wytan1300@users.noreply.github.com>
2020-10-03 00:48:54 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br/>HI
+
         </p>
         <a
           className="App-link"
@@ -16,7 +18,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World
+          Hello 
+          
         </a>
       </header>
     </div>

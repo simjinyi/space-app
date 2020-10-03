eec2d29cd0cdb711130438b5c1a974929436859f
simjinyi <38304422+simjinyi@users.noreply.github.com>
2020-10-03 18:34:13 +0800

    update map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Home from "./home/Home";
-import Facts from "./facts/Facts";
-import Form from "./form/form";
+import Test from "./events/test";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <Form />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/test" exact>
+          <Test />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;

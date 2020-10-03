b4908bc6065802863c67bf899fdbd85fe6c1a61c
ysoh29 <72268971+ysoh29@users.noreply.github.com>
2020-10-04 01:07:45 +0800

    Create Home copy.js

---
+++ src/home/Home copy.js
@@ -0,0 +1,56 @@
+import { Box, Typography, Button } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+
+export default function Home() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${counter} times!`;
+  });
+
+  return (
+    <Box display="flex" flexDirection="column">
+      <Box
+        display="flex"
+        flexGrow={1}
+        p={1}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box display="flex" flexDirection="column">
+          <Typography variant="h1">60 Cents Space Hub</Typography>
+          <Button variant="contained" color="primary" style={{ margin: 5 }}>
+            Community Event ... :)
+          </Button>
+          <Button variant="contained" color="primary" style={{ margin: 5 }}>
+            Astronomical Facts !
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ margin: 5 }}
+            onClick={() => {
+              alert("Earth is not flat, pls dreaming!!");
+            }}
+          >
+            For Flat Earthers
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            style={{ margin: 5 }}
+            onClick={() => {
+              setCounter(counter + 1);
+            }}
+          >
+            {counter}
+          </Button>
+        </Box>
+      </Box>
+      <Box display="flex" p={1}>
+        Daily Pluto status updates: Not a planet
+      </Box>
+    </Box>
+  );
+}

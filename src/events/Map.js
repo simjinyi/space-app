f41a5aac2bf688288a643a9f5315c08b04920877
simjinyi <38304422+simjinyi@users.noreply.github.com>
2020-10-04 16:49:33 +0800

    Update Map.js

--- src/events/Map.js
+++ src/events/Map.js
@@ -1,7 +1,15 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
 import firebase from "firebase";
 import configs from "../configs/dbConfigs";
-import { Box, Card, Paper, TextField } from "@material-ui/core";
+import {
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  Fab,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 import mapboxgl from "mapbox-gl";
 import axios from "axios";
 
@@ -10,9 +18,16 @@ export default function Map() {
     firebase.initializeApp(configs);
   }
 
+  let markers = [];
+
   const [data, setData] = useState([]);
+  const [inputSearchLocation, setInputSearchLocation] = useState(null);
+  const [drawerOpened, setDrawerOpened] = useState(false);
+  const [searchState, setSearchState] = useState(false);
+
   const mapContainerRef = useRef(null);
   let map = null;
+
   const ACCESS_TOKEN =
     "pk.eyJ1Ijoic2ltamlueWkiLCJhIjoiY2tmdGZ5azh5MGh0ajJzcXEydGUyYzhhaCJ9.CyiAKk9np2yG6S3TE60joA";
 
@@ -21,7 +36,9 @@ export default function Map() {
     db.collection("test").onSnapshot((snapshot) => {
       setData(snapshot.docs.map((doc, idx) => ({ key: idx, ...doc.data() })));
     });
+  }, []);
 
+  useEffect(() => {
     mapboxgl.accessToken = ACCESS_TOKEN;
 
     map = new mapboxgl.Map({
@@ -29,7 +46,7 @@ export default function Map() {
       // See style options here: https://docs.mapbox.com/api/maps/#styles
       style: "mapbox://styles/mapbox/streets-v11",
       center: [100.209709, 5.438184],
-      zoom: 12.5,
+      zoom: 10,
     });
 
     map.addControl(
@@ -44,27 +61,28 @@ export default function Map() {
     // add navigation control (the +/- zoom buttons)
     map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
 
-    // clean up on unmount
-    return () => map.remove();
-  }, []);
+    for (let i = 0; i < markers.length; i++) markers.remove();
+    markers = [];
 
-  setTimeout(() => {
     axios
       .get(
-        `https://api.mapbox.com/geocoding/v5/mapbox.places/Penang.json?access_token=${ACCESS_TOKEN}`
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${inputSearchLocation}.json?access_token=${ACCESS_TOKEN}`
       )
       .then((res) => {
-        console.log(res.data.features);
         let marker = new mapboxgl.Marker()
           .setLngLat(res.data.features[1].geometry.coordinates)
           .addTo(map);
 
-        map.center = res.data.features[1].geometry.coordinates;
+        map.flyTo({ center: res.data.features[1].geometry.coordinates });
+        markers.push(marker);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, 1000);
+
+    // clean up on unmount
+    return () => map.remove();
+  }, [searchState]);
 
   return (
     <>
@@ -80,41 +98,68 @@ export default function Map() {
           className="map-container"
           ref={mapContainerRef}
         />
-        <Box
-          display="flex"
-          position="absolute"
+        <Fab
+          variant="extended"
+          onClick={() => setDrawerOpened(true)}
+          color="primary"
+          aria-label="add"
           style={{
-            height: "100%",
-            overflowY: "scroll",
+            position: "absolute",
+            bottom: 30,
+            right: 30,
             zIndex: 999,
-            scrollbarWidth: 0,
           }}
         >
-          <Paper
+          <p>Add Item</p>
+        </Fab>
+        <Drawer
+          anchor="right"
+          open={drawerOpened}
+          onClose={() => setDrawerOpened(false)}
+        >
+          <Box
             display="flex"
             flex={1}
+            style={{ padding: 20 }}
             flexDirection="column"
-            alignItems="center"
           >
-            <form noValidate autoComplete="off" style={{ paddingTop: 20 }}>
-              <Box display="flex" flex={1} justifyContent="center">
-                <TextField display="flex" flex={1} label="Search" />
-              </Box>
-            </form>
-            {/* <Card style={{ width: 200, margin: 20 }}>
-              <p>Testing</p>
-            </Card>
-            <Card style={{ width: 200, margin: 20 }}>
-              <p>Testing</p>
-            </Card>
-            <Card style={{ width: 200, margin: 20 }}>
-              <p>Testing</p>
-            </Card>
-            <Card style={{ width: 200, margin: 20 }}>
-              <p>Testing</p>
-            </Card> */}
-          </Paper>
-        </Box>
+            <Box display="flex" justifyContent="center">
+              <Typography variant="h4" component="h2">
+                Search
+              </Typography>
+            </Box>
+            <Box display="flex" flex={1}>
+              <form
+                display="flex"
+                flex={1}
+                noValidate
+                autoComplete="off"
+                style={{ paddingTop: 20 }}
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  setSearchState(!searchState);
+                }}
+              >
+                <TextField
+                  margin="dense"
+                  fullWidth={true}
+                  label="Location"
+                  onChange={(e) => setInputSearchLocation(e.target.value)}
+                />
+                <Button
+                  variant="contained"
+                  fullWidth={true}
+                  onClick={(e) => {
+                    setSearchState(!searchState);
+                  }}
+                >
+                  Search
+                </Button>
+                <Divider />
+              </form>
+            </Box>
+          </Box>
+        </Drawer>
       </Box>
     </>
   );

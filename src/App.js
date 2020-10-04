add4a58d234c7be1fc090beb3920cd4b11920369
simjinyi <38304422+simjinyi@users.noreply.github.com>
2020-10-04 18:42:04 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import React from "react";
 import Home from "./home/Home";
 import Map from "./events/Map";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import { CssBaseline } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/events" exact>
-          <Map />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/events" exact>
+            <Map />
+          </Route>
+        </Switch>
+      </Router>
+      <CssBaseline />
+    </ThemeProvider>
   );
 }
 
